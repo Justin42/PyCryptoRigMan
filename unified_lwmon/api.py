a98bac2ec093e94ad6be92f481a2d3c23f8c3c55
Justin Baldwin <justin.d.baldwin91@gmail.com>
2018-08-04 23:04:39 -0400

    Add Rig, RigMonitor classes to API

--- unified_lwmon/api.py
+++ unified_lwmon/api.py
@@ -2,7 +2,24 @@ from flask import Flask, jsonify
 from flask_classy import FlaskView
 import yaml
 
-class RigsView(FlaskView):
-    def list(self):
-        response = [{'rigs': {'name': 'local'}}]
-        return jsonify(response)
+
+class Rig(object):
+    def __init__(self, config):
+        self.data = dict()
+        self.data['config'] = config
+        return
+
+    def refresh(self):
+        return
+
+
+class RigMonitor(object):
+    def __init__(self):
+        self.rigs = []
+
+    def add_rig(self, rig):
+        self.rigs.append(rig)
+
+    def refresh(self):
+        for rig in self.rigs:
+            rig.refresh()
